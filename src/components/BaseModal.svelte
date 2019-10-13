bef06afe833dac37c5428d8e21713a6394d6f0e1
dennyfauzan <dennyfauzan92@gmail.com>
2019-10-13 13:54:38 +0700

    first commit

---
+++ src/components/BaseModal.svelte
@@ -0,0 +1,86 @@
+<style lang="scss">
+  #modal-container {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    visibility: hidden;
+    opacity: 0;
+    pointer-events: none;
+    transition: all 0.3s;
+
+    &.active {
+      visibility: visible;
+      opacity: 1;
+      pointer-events: auto;
+    }
+  }
+
+  .overlay {
+    background-color: rgb(0, 0, 0, 0.4);
+    width: 100%;
+    height: 100%;
+  }
+
+  .modal-box {
+    width: 60%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 2em;
+    background: #ffffff;
+    border-left: 6px solid #c81322;
+    box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 1px 10px 0 rgba(0, 0, 0, 0.12),
+      0 2px 4px -1px rgba(0, 0, 0, 0.3);
+    z-index: 10;
+  }
+  .modal-close {
+    color: #aaa;
+    line-height: 50px;
+    font-size: 80%;
+    position: absolute;
+    right: 0;
+    text-align: center;
+    top: 0;
+    width: 70px;
+    text-decoration: none;
+    z-index: 900;
+  }
+  p {
+    color: gray;
+  }
+</style>
+
+<script>
+  let modalToggle = false;
+  function handleModalToggle() {
+    modalToggle = !modalToggle;
+  }
+  function closeModal() {
+    modalToggle = false;
+  }
+</script>
+
+<button on:click={handleModalToggle}>State: {modalToggle}</button>
+<h1 on:click={()=> console.log('runn')}>
+  Sweet Candy
+</h1>
+<div id="modal-container" class={modalToggle? 'active': ''} >
+  <div class="overlay" on:click={()=> modalToggle = false}></div>
+	<div class="modal-box">
+		<span
+      title="Close"
+      class="modal-close"
+      on:click={closeModal}>
+      Close
+    </span> 
+		<h1>
+			Hello Modal
+		</h1>
+		<p>
+			this is the content of modal that can reusable!
+		</p>
+	</div>
+</div>
---
+++ webpack.config.js
@@ -0,0 +1,147 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const mode = process.env.NODE_ENV || 'development';
+const prod = mode === 'production';
+
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project 
+  content: [
+    './src/**/*.html',
+    './src/**/*.svelte',
+    // './src/**/*.jsx',
+    // etc.
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+
+
+function recursiveIssuer(m) {
+  if (m.issuer) {
+    return recursiveIssuer(m.issuer);
+  } else if (m.name) {
+    return m.name;
+  } else {
+    return false;
+  }
+}
+
+module.exports = {
+	entry: {
+		bundle: './src/main.js',
+		common: path.resolve(__dirname, 'src/styles/common.scss'),
+	},
+	resolve: {
+		alias: {
+			svelte: path.resolve('node_modules', 'svelte')
+		},
+		extensions: ['.mjs', '.js', '.svelte', '.scss'],
+		mainFields: ['svelte', 'browser', 'module', 'main']
+	},
+	output: {
+		path: __dirname + '/public',
+		filename: '[name].js',
+		chunkFilename: '[name].[id].js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.svelte$/,
+				use: {
+					loader: 'svelte-loader',
+					options: {
+						emitCss: true,
+						hotReload: true,
+						preprocess: require('svelte-preprocess')({
+							scss: {
+								includePaths: ['src'],
+							},
+							postcss: {
+								plugins: [
+									require('autoprefixer')
+								],
+							},
+						})
+					}
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [
+					/**
+					 * MiniCssExtractPlugin doesn't support HMR.
+					 * For developing, use 'style-loader' instead.
+					 * */
+					prod ? MiniCssExtractPlugin.loader : 'style-loader',
+					'css-loader',
+				]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					/**
+					 * MiniCssExtractPlugin doesn't support HMR.
+					 * For developing, use 'style-loader' instead.
+					 * */
+					// prod ? MiniCssExtractPlugin.loader : 'style-loader',
+					{
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // only enable hot in development
+              hmr: process.env.NODE_ENV === 'development',
+              // if hmr does not work, this is a forceful method.
+              reloadAll: true,
+            },
+          },
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							plugins: [
+								require('tailwindcss'),
+								require('autoprefixer'),
+								...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+							],
+						}
+					},
+					{
+            loader: 'sass-loader',
+            options: {
+              // Prefer `dart-sass`
+							implementation: require('sass'),
+							sassOptions: {
+								includePaths: [path.resolve(__dirname, "src/styles/common.scss")]
+							},
+            },
+					},
+				]
+			},
+		]
+	},
+	mode,
+	optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commonStyles: {
+          name: 'common',
+          test: (m, c, entry = 'common') =>
+            m.constructor.name === 'CssModule' && recursiveIssuer(m) === entry,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+		},
+		minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+		})
+	],
+	devtool: prod ? false: 'source-map'
+};
